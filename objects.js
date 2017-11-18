fe8a5a3e87ed6bab346fbc9129ffb668119d4e2b
Louis Pazos <louispazos@gmail.com>
2017-11-18 07:23:59 +0000

    adding new features, buttons, dropdown, some code

--- objects.js
+++ objects.js
@@ -1,32 +1,54 @@
 var rental = {
 name: "Enterprise Rent A Car",
+allCars: 270,
+allCarsBooked: 0,
 economyCars: 90,
 economyCarsBooked: 0,
 midsizeCars: 180,
 midsizeCarsBooked: 0,
-carClass: ["Economy", "Midsize"],
+// carClass: ["Economy", "Midsize"],
 rentMid: function() {
    document.getElementById("midszAvail").innerHTML = --this.midsizeCars;
 },
+bookedMid: function() {
+   document.getElementById("econAvail").innerHTML = ++this.midsizeCarsBooked;
+},
+
 rentEcon: function() {
    document.getElementById("econAvail").innerHTML = --this.economyCars;
 },
-bookedEcon: function() {
-   document.getElementById("econAvail").innerHTML = ++this.economyCarsBooked;
-},
-bookedMid: function() {
-   document.getElementById("econAvail").innerHTML = ++this.midsizeCarsBooked;
-}
 
+rentAll: function() {
+   document.getElementById("allca").innerHTML = --this.allCars;
+},
+
+bookedAll: function() {
+   document.getElementById("allca").innerHTML = ++this.allCars;
+}
 };
 
 function updateAvail() {
    document.getElementById("midszAvail").innerHTML = rental.midsizeCars;
    document.getElementById("econAvail").innerHTML = rental.economyCars;
+   document.getElementById("allca").innerHTML = rental.allCars;
+   // document.getElementById("allca").innerHTML = rental.allCars;
+
 }
 
 function staticValues() {
    document.getElementById("name").innerHTML = rental.name;
    document.getElementById("econ").innerHTML = rental.carClass[0];
    document.getElementById("midsz").innerHTML = rental.carClass[1];
+   document.getElementById("allca").innerHTML = rental.allCars;
 }
+
+function typesOfCars() {
+   document.getElementById("Accent").innerHTML = "#10, $50 per day";
+}
+
+document.getElementById("Accent").onclick = function(){typesOfCars()};
+
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// }
