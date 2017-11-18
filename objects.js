ea77a321001f1ad45fcd672c295397172fb81213
Louis Pazos <louispazos@gmail.com>
2017-11-18 18:23:01 +0000

    practice1118

--- objects.js
+++ objects.js
@@ -1,54 +1,68 @@
 var rental = {
-name: "Enterprise Rent A Car",
-allCars: 270,
-allCarsBooked: 0,
+// name: "Enterprise Rent A Car",
+// allCars: 270,
+// allCarsBooked: 0,
 economyCars: 90,
-economyCarsBooked: 0,
+// economyCarsBooked: 0,
+economyCarsPrice: 50,
 midsizeCars: 180,
-midsizeCarsBooked: 0,
+// midsizeCarsBooked: 0,
+midsizeCarsPrice: 60
+}
+
 // carClass: ["Economy", "Midsize"],
-rentMid: function() {
-   document.getElementById("midszAvail").innerHTML = --this.midsizeCars;
-},
-bookedMid: function() {
-   document.getElementById("econAvail").innerHTML = ++this.midsizeCarsBooked;
-},
+// rentMid: function() {
+//    document.getElementById("midszAvail").innerHTML = --this.midsizeCars;
+// },
+// bookedMid: function() {
+//    document.getElementById("econAvail").innerHTML = ++this.midsizeCarsBooked;
+// },
 
-rentEcon: function() {
-   document.getElementById("econAvail").innerHTML = --this.economyCars;
-},
+// rentEcon: function() {
+//    document.getElementById("econAvail").innerHTML = --this.economyCars;
+// },
 
-rentAll: function() {
-   document.getElementById("allca").innerHTML = --this.allCars;
-},
+// rentAll: function() {
+//    document.getElementById("allca").innerHTML = --this.allCars;
+// },
 
-bookedAll: function() {
-   document.getElementById("allca").innerHTML = ++this.allCars;
-}
-};
-
-function updateAvail() {
-   document.getElementById("midszAvail").innerHTML = rental.midsizeCars;
-   document.getElementById("econAvail").innerHTML = rental.economyCars;
-   document.getElementById("allca").innerHTML = rental.allCars;
-   // document.getElementById("allca").innerHTML = rental.allCars;
-
-}
-
-function staticValues() {
-   document.getElementById("name").innerHTML = rental.name;
-   document.getElementById("econ").innerHTML = rental.carClass[0];
-   document.getElementById("midsz").innerHTML = rental.carClass[1];
-   document.getElementById("allca").innerHTML = rental.allCars;
-}
-
-function typesOfCars() {
-   document.getElementById("Accent").innerHTML = "#10, $50 per day";
-}
-
-document.getElementById("Accent").onclick = function(){typesOfCars()};
-
-
-// function myFunction() {
-//     document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// bookedAll: function() {
+//    document.getElementById("allca").innerHTML = ++this.allCars;
 // }
+// };
+
+// function updateAvail() {
+//    document.getElementById("midszAvail").innerHTML = rental.midsizeCars;
+//    document.getElementById("econAvail").innerHTML = rental.economyCars;
+//    document.getElementById("allca").innerHTML = rental.allCars;
+//    // document.getElementById("allca").innerHTML = rental.allCars;
+// }
+
+// function staticValues() {
+//    document.getElementById("name").innerHTML = rental.name;
+//    document.getElementById("econ").innerHTML = rental.carClass[0];
+//    document.getElementById("midsz").innerHTML = rental.carClass[1];
+//    document.getElementById("allca").innerHTML = rental.allCars;
+
+// }
+
+// function showFunc() {
+// var class = document.getElementById("inside").value;
+// if (class = "economyClass") {
+//    document.getElementById("econ").innerHTML = rental.carClass[0];
+//    } else if (class = "midsizeClass") { 
+//       document.getElementById("midsz").innerHTML = rental.carClass[1];
+//    } 
+// }
+
+// // other type of function
+// function typesOfCars() {
+//    document.getElementById("Accent").innerHTML = "#10, $50 per day";
+// //    document.getElementById("Fiesta").innerHTML = "#11, $45 per day";
+// //    document.getElementById("Yaris").innerHTML = "#12, $40 per day";
+// // }
+
+// document.getElementById("Accent").onclick = function(){typesOfCars()};
+
+
+
