13298e8402545b89277352bdb802cfea9a17ec04
Louis Pazos <louispazos@gmail.com>
2017-11-20 20:33:50 +0000

    making the dropdown to work

--- objects.js
+++ objects.js
@@ -1,68 +1,76 @@
 var rental = {
-// name: "Enterprise Rent A Car",
-// allCars: 270,
-// allCarsBooked: 0,
+name: "Enterprise Rent A Car",
+allCars: 270,
+allCarsBooked: 0,
 economyCars: 90,
-// economyCarsBooked: 0,
-economyCarsPrice: 50,
+economyCarsBooked: 0,
 midsizeCars: 180,
-// midsizeCarsBooked: 0,
-midsizeCarsPrice: 60
+midsizeCarsBooked: 0,
+carClass: ["Economy", "Midsize"],
+rentMid: function() {
+   document.getElementById("midszAvail").innerHTML = --this.midsizeCars;
+},
+// bookedMid: function() {
+//    document.getElementById("midszAvail").innerHTML = ++this.midsizeCarsBooked;
+// },
+
+rentEcon: function() {
+   document.getElementById("econAvail").innerHTML = --this.economyCars;
+},
+
+rentAll: function() {
+   document.getElementById("allca").innerHTML = --this.allCars;
+},
+
+bookedAll: function() {
+   document.getElementById("allca").innerHTML = ++this.allCars;
+}
+};
+
+function updateAvail() {
+   document.getElementById("midszAvail").innerHTML = rental.midsizeCars;
+   document.getElementById("econAvail").innerHTML = rental.economyCars;
+   document.getElementById("allca").innerHTML = rental.allCars;
+   // document.getElementById("allca").innerHTML = rental.allCars;
+
 }
 
-// carClass: ["Economy", "Midsize"],
-// rentMid: function() {
-//    document.getElementById("midszAvail").innerHTML = --this.midsizeCars;
-// },
-// bookedMid: function() {
-//    document.getElementById("econAvail").innerHTML = ++this.midsizeCarsBooked;
-// },
+function staticValues() {
+   document.getElementById("name").innerHTML = rental.name;
+   document.getElementById("econ").innerHTML = rental.carClass[0];
+   document.getElementById("midsz").innerHTML = rental.carClass[1];
+   document.getElementById("allca").innerHTML = rental.allCars;
+}
 
-// rentEcon: function() {
-//    document.getElementById("econAvail").innerHTML = --this.economyCars;
-// },
+function typesOfCars() {
+   document.getElementById("Accent").innerHTML = "#10, $50 per day";
+}
 
-// rentAll: function() {
-//    document.getElementById("allca").innerHTML = --this.allCars;
-// },
+document.getElementById("Accent").onclick = function(){typesOfCars()};
 
-// bookedAll: function() {
-//    document.getElementById("allca").innerHTML = ++this.allCars;
-// }
-// };
 
-// function updateAvail() {
-//    document.getElementById("midszAvail").innerHTML = rental.midsizeCars;
-//    document.getElementById("econAvail").innerHTML = rental.economyCars;
-//    document.getElementById("allca").innerHTML = rental.allCars;
-//    // document.getElementById("allca").innerHTML = rental.allCars;
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
 // }
 
-// function staticValues() {
-//    document.getElementById("name").innerHTML = rental.name;
-//    document.getElementById("econ").innerHTML = rental.carClass[0];
-//    document.getElementById("midsz").innerHTML = rental.carClass[1];
-//    document.getElementById("allca").innerHTML = rental.allCars;
-
-// }
-
-// function showFunc() {
-// var class = document.getElementById("inside").value;
-// if (class = "economyClass") {
-//    document.getElementById("econ").innerHTML = rental.carClass[0];
-//    } else if (class = "midsizeClass") { 
-//       document.getElementById("midsz").innerHTML = rental.carClass[1];
-//    } 
-// }
-
-// // other type of function
-// function typesOfCars() {
-//    document.getElementById("Accent").innerHTML = "#10, $50 per day";
-// //    document.getElementById("Fiesta").innerHTML = "#11, $45 per day";
-// //    document.getElementById("Yaris").innerHTML = "#12, $40 per day";
-// // }
-
-// document.getElementById("Accent").onclick = function(){typesOfCars()};
+// <<<hidden function>>>
+// function showhide(id) {
+//        	var e = document.getElementById(id);
+//        	e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+//     }
+     
 
 
-
+function showFunc() {
+   var drop = document.getElementById("carType").value;
+   if (drop ="economico") {
+            document.getElementById("econAvail").innerHTML = rental.economyCars;
+   } else {
+      (drop ="mediano")
+      document.getElementById("midszAvail").innerHTML = rental.midsizeCars;
+   // } else { 
+   //    (drop ==="nada")
+      
+   
+}
+}
